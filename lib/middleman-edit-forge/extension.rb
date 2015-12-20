57071c38516fccb085a51c6ddcc26959c23c3820
Michael Scherer <misc@redhat.com>
2015-12-20 23:43:10 +0100

    Add gitlab support

--- lib/middleman-edit-forge/extension.rb
+++ lib/middleman-edit-forge/extension.rb
@@ -3,7 +3,17 @@ module Middleman
     class EditForgeExtension < Extension
       option :forge, 'github', 'Forge name'
       option :project, 'example/example-site', 'Project name'
-      
+      FORGES = {
+        'github' => {
+          'url'  => 'https://github.com/%{project}/edit/master%{source_file}',
+          'name' => 'GitHub',
+        },
+        'gitlab' => {
+          'url'  => 'https://gitlab.com/%{project}/edit/master%{source_file}',
+          'name' => 'GitLab',
+        },
+      }
+
       def after_configuration
         app.set :edit_forge_id, options.forge
         app.set :edit_forge_project, options.project
@@ -14,10 +24,8 @@ module Middleman
         def insert_link_edit_forge
             icon = '<i class="icon fa fa-pencil"></i>'
             source_file = current_page.source_file.sub(root, '')
-            # TODO do not hardcode
-            forge_url = "https://github.com/#{edit_forge_project}/edit/master#{source_file}"
-            # TODO do not hardcode    
-            forge_name = 'GitHub'
+            forge_url = FORGES[edit_forge_id]['url'] % { :project => edit_forge_project, :source_file => source_file }
+            forge_name = FORGES[edit_forge_id]['name']
             link_to "#{icon}Edit this page on #{forge_name}", forge_url, {target: '_blank'}
         end
       end
